d3e96c4f6a2019504deb1739b0567dfb5ea9a0d5
Muhammad Abdullah Usman <abdullahuthman@protonmail.com>
2025-12-28 16:46:36 +0500

    clean up imports

--- Map/src/main/java/com/example/demo/HelloApplication.java
+++ Map/src/main/java/com/example/demo/HelloApplication.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    @Override
+//    @Override
     public void start(Stage stage) throws IOException {
         // Note: No leading slash. This looks in the same package as this class.
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -27,5 +27,3 @@ public class HelloApplication extends Application {
         launch();
     }
 }
-
-//changes
